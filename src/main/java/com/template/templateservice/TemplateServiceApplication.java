b7bf825d4506a097542d92a0439ba210ee754327
Abd-Alkrhman Alkraien <abdlarhmanalkraien@gmail.com>
2022-03-26 17:50:50 +0300

    the templete service register on eureka now

--- src/main/java/com/template/templateservice/TemplateServiceApplication.java
+++ src/main/java/com/template/templateservice/TemplateServiceApplication.java
@@ -2,8 +2,10 @@ package com.template.templateservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class TemplateServiceApplication {
 
     public static void main(String[] args) {
